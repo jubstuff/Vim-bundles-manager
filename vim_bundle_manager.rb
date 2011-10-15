53895f0b017ef4d3e8a4baabb22556ed6e147fb8
Giustino Borzacchiello <giustinob@gmail.com>
2011-10-15 11:12:54 +0200

    Removed constant GIT_BUNDLE

--- vim_bundle_manager.rb
+++ vim_bundle_manager.rb
@@ -4,39 +4,10 @@ require 'fileutils'
 
 class VimBundleManager
 
-  # CONSTANTS
-  
-  GIT_BUNDLES = [ 
-    # "git://github.com/astashov/vim-ruby-debugger.git",
-    # "git://github.com/ervandew/supertab.git",
-    "git://github.com/godlygeek/tabular.git",
-    # "git://github.com/hallison/vim-rdoc.git",
-    # "git://github.com/msanders/snipmate.vim.git",
-    # "git://github.com/pangloss/vim-javascript.git",
-    # "git://github.com/scrooloose/nerdtree.git",
-    # "git://github.com/timcharper/textile.vim.git",
-    # "git://github.com/tpope/vim-cucumber.git",
-    # "git://github.com/tpope/vim-fugitive.git",
-    # "git://github.com/tpope/vim-git.git",
-    # "git://github.com/tpope/vim-haml.git",
-    # "git://github.com/tpope/vim-markdown.git",
-    # "git://github.com/tpope/vim-rails.git",
-    # "git://github.com/tpope/vim-repeat.git",
-    "git://github.com/tpope/vim-surround.git",
-    # "git://github.com/tpope/vim-vividchalk.git",
-    "git://github.com/tsaleh/taskpaper.vim.git",
-    "git://github.com/tsaleh/vim-matchit.git",
-    "git://github.com/tsaleh/vim-shoulda.git",
-    "git://github.com/tsaleh/vim-tcomment.git",
-    # "git://github.com/tsaleh/vim-tmux.git",
-    # "git://github.com/vim-ruby/vim-ruby.git",
-    # "git://github.com/vim-scripts/Gist.vim.git",
-    # "git://github.com/joestelmach/javaScriptLint.vim.git",
-  ]
-
   def initialize(vim_home_path="")
     user = ENV["USER"]
     vim_home = vim_home_path.empty? ? "/home/#{user}/vimtest" : vim_home_path
+
     @git_bundles = create_bundle_hash
 
     @bundles_dir = File.join(vim_home, "bundle")
@@ -45,7 +16,7 @@ class VimBundleManager
 
   def install( bundle_to_install )
     if bundle_to_install == :all
-      GIT_BUNDLES.each do |url|
+      @git_bundles.values.each do |url|
         clone_bundle(url)
       end
     end
@@ -66,7 +37,7 @@ class VimBundleManager
   end
 
   def list_bundles( which=:enabled )
-    all = GIT_BUNDLES.map { |r| get_bundle_name(r) }
+    all = @git_bundles.keys
     enabled = Dir["*"]
     disabled = all - enabled
     case which
